9590bea039a663342a89fa49465ad0791cee9468
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-03-10 14:04:50 -0300

    Nome

---
+++ Aula1/src/nome.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class nome {
+public static void main(String[] args) {
+    String nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+
+    JOptionPane.showMessageDialog(null, "Olá, "+nome);
+  }
+}
