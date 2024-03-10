a7865e5b5ced8107cf57315e95165ae0bf6ca2b0
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-03-10 15:25:48 -0300

    nome completo

---
+++ Aula1/src/sobrenome.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class sobrenome {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+        String sobrenome = JOptionPane.showInputDialog("Qual é o seu sobrenome?");
+        String nomeCompleto = nome + " " + sobrenome;
+
+        JOptionPane.showMessageDialog(null, "Olá " + nomeCompleto);
+    }
+}
