dbcbbdb413c65b9df621773f7a5dc80200a819a0
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-03-10 17:17:57 -0300

    dias da semana

---
+++ Aula3/src/diaSemana.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class diaSemana {
+    public static void main(String[] args) {
+        boolean tudoCerto = false;
+        do {
+            try {
+                int dia = Integer.parseInt(JOptionPane.showInputDialog("Informe um numero entre 1 e 7"));
+                String[] semana = {
+                        "Domingo",
+                        "Segunda-feira",
+                        "Terça-feira",
+                        "Quarta-feira",
+                        "Quinta-feira",
+                        "Sexta-feira",
+                        "Sabado"
+                };
+                JOptionPane.showMessageDialog(null, "Hoje é " + semana[dia - 1]);
+                tudoCerto = true;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Por favor insira um numero valido");
+            }
+
+        } while (tudoCerto == false);
+
+    }
+}
