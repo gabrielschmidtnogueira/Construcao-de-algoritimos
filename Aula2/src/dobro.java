9a4177f9ac66665beb375659c219904da3cb484e
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-03-10 15:55:32 -0300

    aula 2

---
+++ Aula2/src/dobro.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class dobro {
+    public static void main(String[] args) throws Exception {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("insira um numero"));
+        JOptionPane.showMessageDialog(null, "O dobro de " + numero + " é " + numero * 2);
+    }
+}
