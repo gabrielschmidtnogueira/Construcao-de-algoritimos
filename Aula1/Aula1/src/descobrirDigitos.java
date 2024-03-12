1d5157888de101aa43e1d68b1313a5f6fd6d8b11
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-03-12 16:07:08 -0300

    progresso

--- Aula1/Aula1/src/descobrirDigitos.java
+++ Aula1/Aula1/src/descobrirDigitos.java
@@ -2,54 +2,37 @@ import javax.swing.JOptionPane;
 
 public class descobrirDigitos {
     public static void main(String[] args) {
-        String cpf = JOptionPane.showInputDialog("Insira o cpf (apenas numeros)");
+        String cpf = JOptionPane
+                .showInputDialog("Insira o cpf: (coloque um x no digito faltando. Exemplo 123456x8909)");
         int[] numerosCpf = new int[11];
+        int posLetra;
 
-        int digito1;
-        int digito2;
+        posLetra = ProcessaCpf(cpf, numerosCpf);
 
-        int soma = 0;
-        int resto = 0;
-
-        if (cpf.length() != 9) {
-            JOptionPane.showMessageDialog(null, "Digite 9 digitos");
-            return;
+        if (posLetra > 9) {
+            System.out.println(posLetra);
         }
-        for (int i = 0; i < 9; i++) {
-            char digitoChar = cpf.charAt(i);
-            if (Character.isDigit(digitoChar)) {
-                numerosCpf[i] = Character.getNumericValue(digitoChar);
-            } else {
-                JOptionPane.showMessageDialog(null, "por favor insira apenas numeros");
-                return;
+
+    }
+
+    public static int ProcessaCpf(String cpf, int[] digitosCpf) {
+        int posLetra = -1;
+        for (int i = 0; i < cpf.length(); i++) {
+            char c = cpf.charAt(i);
+            if (Character.isDigit(c)) {
+                digitosCpf[i] = Character.getNumericValue(c);
+            } else if (Character.isLetter(c)) {
+                digitosCpf[i] = 0;
+                posLetra = i;
+
             }
         }
-
-        for (int i = 0; i < 9; i++) {
-            soma += numerosCpf[i] * (10 - i);
-        }
-
-        resto = soma % 11;
-        digito1 = 11 - resto;
-
-        if (resto == 1 || resto == 0) {
-            digito1 = 0;
-        }
-        numerosCpf[9] = digito1;
-
-        soma = 0;
-        for (int i = 0; i < 10; i++) {
-            soma += numerosCpf[i] * (11 - i);
-        }
-        resto = soma % 11;
-        digito2 = 11 - resto;
-        if (resto == 1 || resto == 0) {
-            digito2 = 0;
-        }
-        numerosCpf[10] = digito2;
-
-        cpf += numerosCpf[9];
-        cpf += numerosCpf[10];
-        JOptionPane.showMessageDialog(null, cpf);
+        return posLetra;
     }
+
+    public static int UltimosDigitos(String cpf, int[] digitosCpf) {
+        int digito = -1;
+        return digito;
+    }
+
 }
