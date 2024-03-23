b186e1d8e90edb336badddd46d534e3386ad0f48
Gabriel Schmidt Nogueira <115893987+gabrielschmidtnogueira@users.noreply.github.com>
2024-03-22 21:41:40 -0300

    ac1

---
+++ ac1/promocao.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class promocao {
+    public static void main(String[] args) {
+        double preco = Double.parseDouble(JOptionPane.showInputDialog("Informe o preço original do produto"));
+        double desconto = Double.parseDouble(JOptionPane.showInputDialog("Informe o desconto"));
+        String precoFinal = String.format("%.2f", preco - (preco * (desconto / 100)));
+
+        JOptionPane.showMessageDialog(null, "R$ " + precoFinal);
+
+    }
+}
