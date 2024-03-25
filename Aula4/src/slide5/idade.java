f1c30613fe9d4d31dab31afc2413f993758f482e
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-03-24 22:01:26 -0300

    Revert "update"
    This reverts commit c1f79a97029c172fe9bdb7d9d5e5a347dda225ab.

--- Aula4/src/slide5/idade.java
+++ Aula4/src/slide5/idade.java
@@ -3,14 +3,14 @@ import javax.swing.JOptionPane;
 public class idade {
     public static void main(String[] args) {
         int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual é a sua idade?"));
-        if (idade < 16) {
-            JOptionPane.showMessageDialog(null, "voce ainda não pode votar");
-        } else if (idade < 18) {
-            JOptionPane.showMessageDialog(null, "o seu voto é vacultativo");
-        } else if (idade < 60) {
-            JOptionPane.showMessageDialog(null, "o seu voto é obrigatorio");
-        } else {
-            JOptionPane.showMessageDialog(null, "o seu voto é vacultativo");
-        }
+        // if (idade < 16) {
+        // JOptionPane.showMessageDialog(null, "voce ainda não pode votar");
+        // } else if (idade < 18) {
+        // JOptionPane.showMessageDialog(null, "o seu voto é vacultativo");
+        // } else if (idade < 60) {
+        // JOptionPane.showMessageDialog(null, "o seu voto é obrigatorio");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "o seu voto é vacultativo");
+
     }
 }
