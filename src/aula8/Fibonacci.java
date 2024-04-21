d03306bb12b23ebc3263eb483ac184bff5c0f2a9
quadradoid <gabrielnogueiranogueira8@gmail.com>
2024-04-21 15:42:48 -0300

    Troca

--- src/aula8/Fibonacci.java
+++ src/aula8/Fibonacci.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int quatidade = Integer.parseInt(JOptionPane.showInputDialog("Quantos numeros da sequencia vc quer exibir"));
+        int quantidade = Integer.parseInt(JOptionPane.showInputDialog("Quantos numeros da sequencia vc quer exibir"));
         ArrayList<Integer> sequencia = new ArrayList<>();
         int numero = 1;
         int antigoNumero = 1;
         int soma = 0;
-        for (int i = 0; i < quatidade; i++) {
+        for (int i = 0; i < quantidade; i++) {
             sequencia.add(numero);
 
             soma = antigoNumero + numero;
---
+++ src/aula8/Troca.java
@@ -0,0 +1,16 @@
+package aula8;
+
+public class Troca {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
